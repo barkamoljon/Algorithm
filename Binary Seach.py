e153f9c5858cf07d8fc8a3a1ef3d92504ef49fef
Barkamol Urinboev <97020905+barkamoljon@users.noreply.github.com>
2023-03-07 22:16:57 +0500

    Create Binary Seach.py

---
+++ Binary Seach.py
@@ -0,0 +1,15 @@
+def binary_search(list, item):
+  low = 0
+  high = len(list)-1
+  while low <= high
+    mid = (high + low) // 2
+    gues = list[mid]
+    if guess == item
+    return mid
+    if gues > low:
+      high = mid - 1
+    else:
+      low = mod + 1
+  return None
+    
+      
