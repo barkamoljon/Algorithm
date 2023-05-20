2a25fc1993a458032c10a34752a826303e0bcf20
Barkamol Urinboev <97020905+barkamoljon@users.noreply.github.com>
2023-05-20 23:59:12 +0500

    Create knn_classification.py

---
+++ 23_knn_classification/knn_classification.py
@@ -0,0 +1,16 @@
+import math
+def euclidean_distance(x1, y1, x2, y2):
+        """
+    Calculates the Euclidean distance between two points.
+
+    Args:
+        x1: The x-coordinate of the first point.
+        y1: The y-coordinate of the first point.
+        x2: The x-coordinate of the second point.
+        y2: The y-coordinate of the second point.
+
+    Returns:
+        The Euclidean distance between the two points.
+    """
+    
+  
