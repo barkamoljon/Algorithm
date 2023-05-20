37bb3affd7a0a39c4a21569b367143a7976248ff
Barkamol Urinboev <97020905+barkamoljon@users.noreply.github.com>
2023-05-21 00:37:27 +0500

    Update knn_classification.py

--- 23_knn_classification/knn_classification.py
+++ 23_knn_classification/knn_classification.py
@@ -12,5 +12,44 @@ def euclidean_distance(x1, y1, x2, y2):
     Returns:
         The Euclidean distance between the two points.
     """
-    
+    return math.sqrt((x1-x2) ** 2 + (y1 - y2) ** 2)
+
+def find_k_nearest_neighborns(x_train, y_train, x_test, k):
+    """
+    Finds the k nearest neighbors of a point in a dataset.
+
+    Args:
+        x_train: The training data points.
+        y_train: The labels of the training data points.
+        x_test: The point to find the nearest neighbors of.
+        k: The number of nearest neighbors to find.
+
+    Returns:
+        A list of the k nearest neighbors of the point, along with their distances.
+    """
+    distances = []
+        for i in range(len(x_train)):
+            distance = euclidean_distance(x_train[i][0], x_train[i][1], x_test[0], x_test[1])
+            distance.append((distance, y_train[i]))
+                
+                distances.sort()
+                return distances[:k]
+def predict_label(x_test, x_train, y_train, k):
+    """
+    Predicts the label of a point using the KNN algorithm.
+
+    Args:
+        x_test: The point to predict the label of.
+        x_train: The training data points.
+        y_train: The labels of the training data points.
+        k: The number of nearest neighbors to use.
+
+    Returns:
+        The predicted label of the point.
+    """
+    nearest_neighbors = find_k_nearest_neighbors(x_train, y_train, x_test, k)
+    most_common_label = max(nearest_neighbors, key=lambda x:x[1])[1]
+    return most_common_label
+
+        
   
